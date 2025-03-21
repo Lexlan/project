da4a0c429e2fce71ac5abc20e2cde6d0ec7e73ea
Lexlan <93628843+Lexlan@users.noreply.github.com>
2025-03-21 17:51:25 +0000

    wowza

---
+++ main.js
@@ -0,0 +1,7 @@
+let hobbySubClasses = ["work", "education", "entertainment"];
+
+function displaySubClasses {
+    //just to check if it will change the text
+    document.getElementById("subclasses").innerHTML = "HobbySubClasses";
+}
+
